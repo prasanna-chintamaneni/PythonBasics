3bbd1db96be1d6eecdbee581fdee0b0f87b98923
prasanna-chintamaneni <prassu1402@gmail.com>
2023-09-26 23:27:24 +0530

    type of literals

---
+++ literals.py
@@ -0,0 +1,16 @@
+"""Literals are of 3 types
+1. NUMERICAL-->integer, float,octal, hexadecimal
+2.STRING-->can be represented in either single quotes or double quotes
+3. BOOLEAN-->True or False-->Numerical True=1, Numerical False=0
+"""
+a=2
+b=3.0
+print(type(a))
+print(type(b))
+print(type(a+b))
+
+s="Prasanna"
+print(type(s))
+
+x=True
+print(type(x))
