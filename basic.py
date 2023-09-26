269868eb3b211947af53a7a32919c17f89bc42d2
prasanna-chintamaneni <prassu1402@gmail.com>
2023-09-26 23:17:55 +0530

    intro to backslash

--- basic.py
+++ basic.py
@@ -1,3 +1,6 @@
 print("Hello! I am Prasanna Chintamaneni")
 
 print("Hello! I am 'Prasanna Chintamaneni'")#single quotes in double quotes will be considered as special character
+
+print("Hello! \"I am Prasanna Chintamaneni")
+#back slash considers that there is special character after that 
